22de6f06c2279e1b0e75261c295ed62141b29094
Ubuntu <ubuntu@ip-172-31-46-202.us-west-2.compute.internal>
2023-11-20 06:50:35 +0000

    Fix: 선 응답 후 추론

--- routers/creation.py
+++ routers/creation.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, ValidationError
 from typing import List
@@ -16,9 +16,7 @@ class DataPayload(BaseModel):
     imageIdentifiers: list
 
 class DataToSend(BaseModel):
-    label_list: List[str]
-
-instant_classifier = classifier()
+    labels: List[str]
 
 async def get_image_from_cdn(cdn_url: str) -> bytes:
     async with httpx.AsyncClient() as client:
@@ -29,33 +27,46 @@ async def get_image_from_cdn(cdn_url: str) -> bytes:
 
     return response.content
 
+
+async def run_model(image_data: list, BACKEND_URL: str):
+    # AI 모델 추론 수행
+    # 예: result = model.predict(image_data)
+
+    images = await asyncio.gather(*[get_image_from_cdn("https://d1idwg6sscgubz.cloudfront.net/" + cdn_url) for cdn_url in image_data])
+    
+    instant_classifier = classifier()
+    
+    top_probs, label_list = instant_classifier.classify_creation(images)
+    label_list = list(set(label_list))
+    # label_list = ['banana', 'apple', 'train'] # 디버깅용 코드
+
+    data_to_send = DataToSend(labels=label_list)
+    async with httpx.AsyncClient() as client:
+          response = await client.patch(BACKEND_URL, json=data_to_send.dict(), headers={"Authorization": "UserId 5"})
+        #   if response.status_code == 200:
+        #     # 응답 코드가 200일 때, OK를 반환합니다.
+        #     return "OK"
+        #   else:
+        #     # 응답 코드가 200이 아닐 때, 에러 코드와 메시지를 출력합니다.
+        #     error_message = f"Error: {response.status_code}, {response.text}"
+        #     print(error_message)
+        #     return error_message
+
+
 @router.post("/quest/{QUEST_ID}/shot")
-async def receive_data(QUEST_ID: int, payload: DataPayload):
+async def receive_data(QUEST_ID: int, payload: DataPayload, background_tasks: BackgroundTasks):
     try:
         if not payload.imageIdentifiers:  # 리스트가 비어있는지 확인
             raise ValueError("No image identifiers provided")
-        
-        images = await asyncio.gather(*[get_image_from_cdn("https://d1idwg6sscgubz.cloudfront.net/" + cdn_url) for cdn_url in payload.imageIdentifiers])
 
         # 백엔드 서버의 엔드포인트 URL
         BACKEND_URL = os.getenv('BACKEND_URL')  
         BACKEND_URL = BACKEND_URL + "/quest/" + str(QUEST_ID) + "/shot"
+        
+        # 필요한 데이터 처리 및 클라이언트에게 빠른 응답
+        background_tasks.add_task(run_model, payload.imageIdentifiers, BACKEND_URL)
+        return {"message": "Data received, processing in background"}
 
-        # top_probs, label_list = instant_classifier.classify_creation(images)
-        # label_list = list(set(label_list))
-        label_list = ['banana', 'apple', 'train'] # 디버깅용 코드
-
-        data_to_send = DataToSend(label_list=label_list)
-        async with httpx.AsyncClient() as client:
-          response = await client.patch(BACKEND_URL, json=data_to_send.dict(), headers={"Authorization": "UserId 5"})
-          if response.status_code == 200:
-            # 응답 코드가 200일 때, OK를 반환합니다.
-            return "OK"
-          else:
-            # 응답 코드가 200이 아닐 때, 에러 코드와 메시지를 출력합니다.
-            error_message = f"Error: {response.status_code}, {response.text}"
-            print(error_message)
-            return error_message
     except ValidationError as e:
         # 유효성 검사 실패 시 예외 처리
         raise HTTPException(status_code=422, detail=str(e))
--- routers/judgment.py
+++ routers/judgment.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, ValidationError
 from typing import List
@@ -19,8 +19,6 @@ class DataPayload(BaseModel):
 class DataToSend(BaseModel):
   judgement: str
 
-instant_classifier = classifier()
-
 async def get_image_from_cdn(cdn_url: str) -> bytes:
     async with httpx.AsyncClient() as client:
         response = await client.get(cdn_url)
@@ -30,32 +28,40 @@ async def get_image_from_cdn(cdn_url: str) -> bytes:
 
     return response.content
 
+async def run_model(image_data: list, label: str, BACKEND_URL: str):
+    images = await asyncio.gather(*[get_image_from_cdn("https://d1idwg6sscgubz.cloudfront.net/" + cdn_url) for cdn_url in image_data])
+    
+    instant_classifier = classifier()
+    
+    success = instant_classifier.classify_judgment(images, label)
+    # success="SUCCESS" # 디버깅용 코드
+
+    data_to_send = DataToSend(judgement=success)
+    async with httpx.AsyncClient() as client:
+          response = await client.patch(BACKEND_URL, json=data_to_send.dict(), headers={"Authorization": "UserId 5"})
+        #   if response.status_code == 200:
+        #     # 응답 코드가 200일 때, OK를 반환합니다.
+        #     return "OK"
+        #   else:
+        #     # 응답 코드가 200이 아닐 때, 에러 코드와 메시지를 출력합니다.
+        #     error_message = f"Error: {response.status_code}, {response.text}"
+        #     print(error_message)
+        #     return error_message
+
 @router.post("/post/{POST_ID}/judge")
-async def receive_data(POST_ID: int, payload: DataPayload):
+async def receive_data(POST_ID: int, payload: DataPayload, background_tasks: BackgroundTasks):
     try:
         if not payload.imageIdentifiers:  # 리스트가 비어있는지 확인
-          raise ValueError("No image identifiers provided") 
-
-        images = await asyncio.gather(*[get_image_from_cdn("https://d1idwg6sscgubz.cloudfront.net/" + cdn_url) for cdn_url in payload.imageIdentifiers])
+            raise ValueError("No image identifiers provided")
 
         # 백엔드 서버의 엔드포인트 URL
         BACKEND_URL = os.getenv('BACKEND_URL')  
         BACKEND_URL = BACKEND_URL + "/post/" + str(POST_ID) + "/judge"
-
-        # success = instant_classifier.classify_judgment(images, payload.label)
-        success="SUCCESS" # 디버깅용 코드
         
-        data_to_send = DataToSend(judgement=success)
-        async with httpx.AsyncClient() as client:
-          response = await client.patch(BACKEND_URL, json=data_to_send.dict(), headers={"Authorization": "UserId 5"})
-          if response.status_code == 200:
-            # 응답 코드가 200일 때, OK를 반환합니다.
-            return "OK"
-          else:
-            # 응답 코드가 200이 아닐 때, 에러 코드와 메시지를 출력합니다.
-            error_message = f"Error: {response.status_code}, {response.text}"
-            print(error_message)
-            return error_message
+        # 필요한 데이터 처리 및 클라이언트에게 빠른 응답
+        background_tasks.add_task(run_model, payload.imageIdentifiers, payload.label, BACKEND_URL)
+        return {"message": "Data received, processing in background"}
+
     except ValidationError as e:
         # 유효성 검사 실패 시 예외 처리
         raise HTTPException(status_code=422, detail=str(e))
