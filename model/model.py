6f38b8c0f4b8a6384dbdd2e324a9f6141c2c9dd4
Ubuntu <ubuntu@ip-172-31-46-202.us-west-2.compute.internal>
2023-11-29 05:04:03 +0000

    Fix: label 추가, model 오류 수정

--- model/model.py
+++ model/model.py
@@ -1,7 +1,8 @@
 import numpy as np
 import torch
 from PIL import Image
-import clip
+# import clip
+import jax
 import json
 import requests
 import io
@@ -12,14 +13,16 @@ from koclip.koclip import load_koclip
 class classifier():
   def __init__(self):
     self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    self.model, self.processor = load_koclip("koclip-large")
+    self.model, self.processor = load_koclip("koclip-base")
 
-    with open("labels.txt", "r") as f:
-      labels = f.read()
-      print(labels)
-    self.text = [f"이것은 {label}이다." for label in labels]
+    LABEL_PATH = os.getenv('LABEL_PATH')  
+    with open(LABEL_PATH, "r") as f:
+      self.labels = f.readlines()
+    self.labels = [label.strip() for label in self.labels]
 
-    self.model.to(self.device).eval()
+    self.text = [f"이것은 {label}이다." for label in self.labels]
+
+    # self.model.to(self.device).eval()
 
   def __del__(self):
     print("메모리를 해제합니다.")
@@ -28,75 +31,86 @@ class classifier():
     images = []
     for src in source:
       image = Image.open(io.BytesIO(src))
-      images.append(self.preprocess(image))
+      images.append(image)
+      # 이미지 사이즈 전처리
     image_input = torch.tensor(np.stack(images)).to(self.device)
 
     with torch.no_grad():
-      inputs = processor(
+      inputs = self.processor(
         text=self.text,
         images=images, 
-        return_tensors="jax", # could also be "pt" 
+        return_tensors="jax",
         padding=True
       )
-      outputs = model(**inputs)
+      outputs = self.model(**inputs)
       probs = jax.nn.softmax(outputs.logits_per_image, axis=1)
 
     top_probs = []
     top_labels = []
     for prob in probs:
-      for idx, prob in sorted(enumerate(prob), key=lambda x: x[1], reverse=True):
-        tmp_probs = []
-        tmp_labels = []
-        tmp_probs.append(prob)
-        tmp_labels.append(self.text[idx])
+      tmp_probs = []
+      tmp_labels = []
+      for idx, p in sorted(enumerate(prob), key=lambda x: x[1], reverse=True):
+        print(idx, p, self.labels[idx])
+        tmp_probs.append(p)
+        tmp_labels.append(idx)
+        if len(tmp_probs) == 5: break
       
       top_probs.append(tmp_probs)
       top_labels.append(tmp_labels)
 
-    label_list = [[label for label in top_labels[i].numpy()] for i in range(len(images))]
+    label_list = [[self.labels[idx] for idx in top_labels[i]] for i in range(len(images))]
     label_list = np.array(label_list).flatten().tolist()
-
+    label_list = list(set(label_list))
+    print(label_list)
     return top_probs, label_list
 
   def classify_judgment(self, source: List[str], label: str):
     images = []
     for src in source:
       image = Image.open(io.BytesIO(src))
-      images.append(self.preprocess(image))
+      images.append(image)
+      # 이미지 사이즈 전처리
     image_input = torch.tensor(np.stack(images)).to(self.device)
     
     with torch.no_grad():
-      inputs = processor(
+      inputs = self.processor(
         text=self.text,
         images=images, 
-        return_tensors="jax", # could also be "pt" 
+        return_tensors="jax",
         padding=True
       )
-      outputs = model(**inputs)
+      outputs = self.model(**inputs)
       probs = jax.nn.softmax(outputs.logits_per_image, axis=1)
 
     top_probs = []
     top_labels = []
     for prob in probs:
-      for idx, prob in sorted(enumerate(prob), key=lambda x: x[1], reverse=True):
-        tmp_probs = []
-        tmp_labels = []
-        tmp_probs.append(prob)
-        tmp_labels.append(self.text[idx])
+      tmp_probs = []
+      tmp_labels = []
+      for idx, p in sorted(enumerate(prob), key=lambda x: x[1], reverse=True):
+        print(idx, p, self.labels[idx])
+        tmp_probs.append(p)
+        tmp_labels.append(idx)
+        if len(tmp_probs) == 5: break
       
       top_probs.append(tmp_probs)
       top_labels.append(tmp_labels)
 
     candidates = []
     for i in range(len(images)):
-      if self.imagenet_labels[top_labels[i][0]] == label:
+      if self.labels[top_labels[i][0]] == label:
         candidates.append(i)
     
     if len(candidates) == 0:
+      print("FAIL")
       return "FAIL"
 
     for candidate in candidates:
       if top_probs[candidate][0] < 0.6 or top_probs[candidate][0] <= 2 * top_probs[candidate][1]:
+        print(top_probs[candidate][0], top_probs[candidate][1])
+        print("FAIL")
         return "FAIL"
       else:
+        print("SUCCESS")
         return "SUCCESS"
