7f74528735c8d32ee598c5ed1b381742c46b6891
Ubuntu <ubuntu@ip-172-31-46-202.us-west-2.compute.internal>
2023-12-02 09:29:53 +0000

    Fix: 한국어 CLIP 모델에서 영어 모델로 변경

--- model/model.py
+++ model/model.py
@@ -1,28 +1,26 @@
 import numpy as np
 import torch
 from PIL import Image
+import clip
 import jax
 import json
 import requests
 import io
 import os
 from typing import List
-from koclip.koclip import load_koclip
 
 class classifier():
   def __init__(self):
     self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    self.model, self.processor = load_koclip("koclip-base")
+    self.model, self.preprocess = clip.load("RN101")
 
-    self.LABEL_PATH = os.getenv('LABEL_PATH')  
-    with open(self.LABEL_PATH, "r") as f:
-      self.labels = f.readlines()
-    self.labels = [label.strip() for label in self.labels]
-
-    self.text = [f"이것은 {label}이다." for label in self.labels]
-
-    # self.model.to(self.device).eval()
-
+    LABEL_URL = os.getenv('LABEL_URL')
+    self.labels_json = json.loads(requests.get(LABEL_URL).text)
+    self.labels = [self.labels_json[str(k)][1] for k in range(len(self.labels_json))]
+    text_descriptions = [f"This is a photo of a {label}" for label in self.labels]
+    self.text_tokens = clip.tokenize(text_descriptions).to(self.device)
+    self.model.to(self.device).eval()
+    
   def __del__(self):
     print("메모리를 해제합니다.")
 
@@ -30,79 +28,40 @@ class classifier():
     images = []
     for src in source:
       image = Image.open(io.BytesIO(src))
-      images.append(image)
-      # 이미지 사이즈 전처리
+      images.append(self.preprocess(image))
     image_input = torch.tensor(np.stack(images)).to(self.device)
 
     with torch.no_grad():
-      inputs = self.processor(
-        text=self.text,
-        images=images, 
-        return_tensors="jax",
-        padding=True
-      )
-      outputs = self.model(**inputs)
-      probs = jax.nn.softmax(outputs.logits_per_image, axis=1)
+      image_features = self.model.encode_image(image_input).float()
+      text_features = self.model.encode_text(self.text_tokens).float()
 
-    top_probs = []
-    top_labels = []
-    for prob in probs:
-      tmp_probs = []
-      tmp_labels = []
-      for idx, p in sorted(enumerate(prob), key=lambda x: x[1], reverse=True):
-        print(idx, p, self.labels[idx])
-        tmp_probs.append(p)
-        tmp_labels.append(idx)
-        if len(tmp_probs) == 5: break
-      
-      top_probs.append(tmp_probs)
-      top_labels.append(tmp_labels)
+      image_features /= image_features.norm(dim=-1, keepdim=True)
+      text_features /= text_features.norm(dim=-1, keepdim=True)
 
-    label_list = [[self.labels[idx] for idx in top_labels[i]] for i in range(len(images))]
+    text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    top_probs, top_labels = text_probs.cpu().topk(5, dim=-1)
+    label_list = [[self.labels[index] for index in top_labels[i].numpy()] for i in range(len(images))]
     label_list = np.array(label_list).flatten().tolist()
-    label_list = list(set(label_list))
     print(label_list)
     return top_probs, label_list
 
   def classify_judgment(self, source: List[str], label: str):
-    # label이 전체 label list에 없을 경우, 추가해준다.
-    if label not in self.labels:
-      with open(self.LABEL_PATH, "a") as f:
-        f.write(label+"\n")
-      self.labels.append(label)
-      self.text = [f"이것은 {label}이다." for label in self.labels]
-
     images = []
     for src in source:
       image = Image.open(io.BytesIO(src))
-      images.append(image)
-      # 이미지 사이즈 전처리
+      images.append(self.preprocess(image))
     image_input = torch.tensor(np.stack(images)).to(self.device)
     
     with torch.no_grad():
-      inputs = self.processor(
-        text=self.text,
-        images=images, 
-        return_tensors="jax",
-        padding=True
-      )
-      outputs = self.model(**inputs)
-      probs = jax.nn.softmax(outputs.logits_per_image, axis=1)
+      image_features = self.model.encode_image(image_input).float()
+      text_features = self.model.encode_text(self.text_tokens).float()
 
-    top_probs = []
-    top_labels = []
-    for prob in probs:
-      tmp_probs = []
-      tmp_labels = []
-      for idx, p in sorted(enumerate(prob), key=lambda x: x[1], reverse=True):
-        print(idx, p, self.labels[idx])
-        tmp_probs.append(p)
-        tmp_labels.append(idx)
-        if len(tmp_probs) == 5: break
-      
-      top_probs.append(tmp_probs)
-      top_labels.append(tmp_labels)
+      image_features /= image_features.norm(dim=-1, keepdim=True)
+      text_features /= text_features.norm(dim=-1, keepdim=True)
 
+    text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+    top_probs, top_labels = text_probs.cpu().topk(5, dim=-1)
+    
     candidates = []
     for i in range(len(images)):
       if self.labels[top_labels[i][0]] == label:
@@ -114,9 +73,21 @@ class classifier():
 
     for candidate in candidates:
       if top_probs[candidate][0] < 0.6 or top_probs[candidate][0] <= 2 * top_probs[candidate][1]:
+        print(self.labels[top_labels[candidate][0]], self.labels[top_labels[candidate][1]])
         print(top_probs[candidate][0], top_probs[candidate][1])
         print("FAIL")
         return "FAIL"
       else:
         print("SUCCESS")
         return "SUCCESS"
+
+  def update_labels(self, label):
+    # label이 전체 label list에 없을 경우, 추가해준다.
+    if label not in self.labels:
+      LABEL_PATH = os.getenv('LABEL_PATH')
+      with open(LABEL_PATH, "a") as f:
+        f.write(label+"\n")
+      self.labels.append(label)
+      text_descriptions = [f"This is a photo of a {label}" for label in self.labels]
+      self.text_tokens = clip.tokenize(text_descriptions).to(self.device)
+      self.model.to(self.device).eval()
