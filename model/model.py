8cb11b148b7a415590b9df3ae1f3676e62470c56
Ubuntu <ubuntu@ip-172-31-46-202.us-west-2.compute.internal>
2023-12-04 03:27:08 +0000

    Fix: 작동 오류 수정 및 레이블 변경

--- model/model.py
+++ model/model.py
@@ -12,17 +12,24 @@ from typing import List
 class classifier():
   def __init__(self):
     self.device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-    self.model, self.preprocess = clip.load("RN101")
+    self.model, self.preprocess = clip.load("ViT-L/14@336px")
 
-    LABEL_URL = os.getenv('LABEL_URL')
-    self.labels_json = json.loads(requests.get(LABEL_URL).text)
-    self.labels = [self.labels_json[str(k)][1] for k in range(len(self.labels_json))]
+    # 외부 레이블 가져오기
+    DATASET = os.getenv('DATASET')
+    with open(DATASET, "r") as f:
+      self.labels = json.load(f)
     self.labels = [item.replace('_', ' ') for item in self.labels]
+
+    # 직접 입력으로 추가된 레이블 가져오기
+    LABEL_PATH = os.getenv('LABEL_PATH')
+    with open(LABEL_PATH, "r") as f:
+      for line in f:
+        self.labels.append(line.strip())
     text_descriptions = [f"This is a photo of a {label}" for label in self.labels]
     self.text_tokens = clip.tokenize(text_descriptions).to(self.device)
     self.model.to(self.device).eval()
 
-    url_for_deepl = 'https://api-free.deepl.com/v2/translate'
+    self.url_for_deepl = 'https://api-free.deepl.com/v2/translate'
     AUTH_KEY = os.getenv('AUTH_KEY')
     self.params = {'auth_key' : AUTH_KEY, 'text' : '', 'source_lang' : 'EN', 'target_lang': 'KO' }
 
@@ -53,9 +60,10 @@ class classifier():
     self.params['text'] = label_list
     self.params['source_lang'] = 'EN' 
     self.params['target_lang'] = 'KO'
-    result = requests.post(url_for_deepl, data=self.params, verify=False)
+    result = requests.post(self.url_for_deepl, data=self.params, verify=False)
     ko_label_list = [result.json()['translations'][i]["text"] for i in range(len(result.json()['translations']))]
-      
+    print(ko_label_list)
+    
     return top_probs, ko_label_list
 
   def classify_judgment(self, source: List[str], label: str):
@@ -63,9 +71,10 @@ class classifier():
     message = label
     self.params['text'] = message
     self.params['source_lang'] = 'KO'
-    self.params['target_lang'] = 'EN'
-    result = requests.post(url_for_deepl, data=self.params, verify=False)
+    self.params['target_lang'] = 'EN-US'
+    result = requests.post(self.url_for_deepl, data=self.params, verify=False)
     label = result.json()['translations'][0]["text"]
+    print(label)
 
     images = []
     for src in source:
@@ -91,24 +100,27 @@ class classifier():
     if len(candidates) == 0:
       print("FAIL")
       return "FAIL"
+    else:
+      print("SUCCESS")
+      return "SUCCESS"
 
-    for candidate in candidates:
-      if top_probs[candidate][0] < 0.6 or top_probs[candidate][0] <= 2 * top_probs[candidate][1]:
-        print(self.labels[top_labels[candidate][0]], self.labels[top_labels[candidate][1]])
-        print(top_probs[candidate][0], top_probs[candidate][1])
-        print("FAIL")
-        return "FAIL"
-      else:
-        print("SUCCESS")
-        return "SUCCESS"
+    # for candidate in candidates:
+    #   if top_probs[candidate][0] < 0.6 or top_probs[candidate][0] <= 2 * top_probs[candidate][1]:
+    #     print(self.labels[top_labels[candidate][0]], self.labels[top_labels[candidate][1]])
+    #     print(top_probs[candidate][0], top_probs[candidate][1])
+    #     print("FAIL")
+    #     return "FAIL"
+    #   else:
+    #     print("SUCCESS")
+    #     return "SUCCESS"
 
   def update_labels(self, label):
     # 한국어 -> 영어
     message = label
     self.params['text'] = message
     self.params['source_lang'] = 'KO'
-    self.params['target_lang'] = 'EN'
-    result = requests.post(url_for_deepl, data=self.params, verify=False)
+    self.params['target_lang'] = 'EN-US'
+    result = requests.post(self.url_for_deepl, data=self.params, verify=False)
     label = result.json()['translations'][0]["text"]
 
     # label이 전체 label list에 없을 경우, 추가해준다.
